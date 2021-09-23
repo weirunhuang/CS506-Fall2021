e3f55fac37d9795d676c2638e5c657a5df77228d
weirunhuang <weirun1367953389@gmail.com>
2021-09-23 16:14:46 -0400

    add library

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -1,3 +1,14 @@
 def draw_library():
-    print("library not found")
+    print("______________________________________")
+    print("I                                    I")
+    print("I             library                I")
+    print("I                                    I")
+    print("I [  ] [  ] [  ] [  ] [  ] [  ] [  ] I")
+    print("I                                    I")
+    print("I              nnnnn                 I")
+    print("I             n .1. n                I")
+    print("I             n  1  n                I")
     return
+
+
+draw_library()
