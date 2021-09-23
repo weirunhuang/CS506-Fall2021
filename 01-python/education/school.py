1de6c35602d3206d6724b703e003e8a6b17d2d12
weirunhuang <weirun1367953389@gmail.com>
2021-09-23 16:04:59 -0400

    add school

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,14 @@
 def draw_school():
-    print("school not found")
+    print("______________________________________")
+    print("I                                    I")
+    print("I             School                 I")
+    print("I                                    I")
+    print("I [  ] [  ] [  ] [  ] [  ] [  ] [  ] I")
+    print("I                                    I")
+    print("I              nnnnn                 I")
+    print("I             n .1. n                I")
+    print("I             n  1  n                I")
+
     return
+
+draw_school()
